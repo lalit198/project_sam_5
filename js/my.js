0898cdad8ae9ff436dda6b8ad83ce48dfc31ef3f
lalit singh <lalit92bora@gmail.com>
2022-08-03 22:44:47 +0530

    First Commit

---
+++ js/my.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+    $('.desk_show_eye1').click(function(){
+   
+    if($(this).hasClass('fa-eye-slash')){
+       
+      $(this).removeClass('fa-eye-slash');
+      
+      $(this).addClass('fa-eye');
+      
+      $('.desk_pass1').attr('type','text');
+        
+    }else{
+     
+      $(this).removeClass('fa-eye');
+      
+      $(this).addClass('fa-eye-slash');  
+      
+      $('.desk_pass1').attr('type','password');
+    }
+});
+});
+
+$(document).ready(function () {
+    $('.desk_show_eye2').click(function(){
+   
+    if($(this).hasClass('fa-eye-slash')){
+       
+      $(this).removeClass('fa-eye-slash');
+      
+      $(this).addClass('fa-eye');
+      
+      $('.desk_pass2').attr('type','text');
+        
+    }else{
+     
+      $(this).removeClass('fa-eye');
+      
+      $(this).addClass('fa-eye-slash');  
+      
+      $('.desk_pass2').attr('type','password');
+    }
+});
+});
+
+$(document).ready(function () {
+  $('.invested_listed').css("display", "none");
+  $('.investlisting_item_one').click(function(){
+  $('.investlisting_item_one').addClass('active'); 
+  $('.investlisting_item_two').removeClass('active');
+  $('.investlisting_item_three').removeClass('active');
+  $('.investlisting_box_parent').css("display", "grid");
+  $('.invested_listed').css("display", "none");
+});
+
+$('.investlisting_item_two').click(function(){
+  $('.investlisting_item_two').addClass('active'); 
+  $('.investlisting_item_one').removeClass('active');
+  $('.investlisting_item_three').removeClass('active');
+  $('.invested_listed').css("display", "block");
+  $('.investlisting_box_parent').css("display", "none");
+});
+
+$('.investlisting_item_three').click(function(){
+  $('.investlisting_item_three').addClass('active'); 
+  $('.investlisting_item_one').removeClass('active');
+  $('.investlisting_item_two').removeClass('active');
+  $('.invested_listed').css("display", "block");
+  $('.investlisting_box_parent').css("display", "none");
+});
+});
